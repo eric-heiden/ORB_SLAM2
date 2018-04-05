8a9dd1681a6f37291677f796c229d4f988840548
eric-heiden <Eric13992>
2018-04-05 12:02:18 +0000

    Alcatraz demo launch file

---
+++ demo_alcatraz.sh
@@ -0,0 +1,2 @@
+./Examples/Monocular/mono_alcatraz  /home/wal/dev/ORB_SLAM2/Vocabulary/ORBvoc.txt /home/wal/dev/ORB_SLAM2/Examples/Monocular/Alcatraz.yaml /home/wal/dev/ORB_SLAM2/Examples/Monocular/alcatraz
+
